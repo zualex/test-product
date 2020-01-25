fdadd20da9cc9a5e44ca00c3bc6ebc064dcb342c
zualex <Azubarev32@yandex.ru>
2020-01-25 17:33:15 +0300

    Create Repository folder

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -7,7 +7,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
  * @ORM\Table(name="products")
  */
 class Product
